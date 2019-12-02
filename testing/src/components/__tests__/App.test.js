9dc281d1610e7a6f473390e3eb28cc3a8372adaa
magic8baller <lturis@gmail.com>
2019-12-02 16:48:37 -0500

    logging innerhtml of created div in test

--- testing/src/components/__tests__/App.test.js
+++ testing/src/components/__tests__/App.test.js
@@ -5,12 +5,14 @@ import App from '../App'
 //global function- dont need to require it
 // it ('test description', () => {let x = 'test logic here'})
 
-it ('shows a comment box', () => {
+it('shows a comment box', () => {
 	const div = document.createElement('div')
 
 	ReactDOM.render(<App />, div)
 	//look inside div, check if comment box is inside it ('shows a comment
 	console.log(div.innerHTML)
+	//  <div style="border: 2px solid green;"><div style="border: 2px solid blue; margin: 2em;">Comment Box</div><div style="border: 2px solid red; margin: 2em;">comment list</div></div>
+
 	ReactDOM.unmountComponentAtNode(div)
 })
 
