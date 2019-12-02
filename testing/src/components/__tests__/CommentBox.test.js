27733e28f1eec4b0214d341a4fe5f1d91c11cc91
magic8baller <lturis@gmail.com>
2019-12-02 18:04:41 -0500

    testing: simulating events in commentbox

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {mount} from 'enzyme'
+import {mount, unmount} from 'enzyme'
 import CommentBox from 'components/CommentBox'
 
 
@@ -9,13 +9,27 @@ let wrapped;
 beforeEach(() => {
 	wrapped = mount(<CommentBox/>)
 })
+
+afterEach(() => {
+	wrapped.unmount()
+})
+
 it('has a textarea and a button', () => {
 	// console.log(wrapped.find("textarea").length);
 	expect(wrapped.find("textarea").length).toEqual(1)
 	expect(wrapped.find("button").length).toEqual(1)
 })
 
-it('allows user to input text into textarea and submit', () => {
+it('allows user to input text into textarea', () => {
+//simulate events:
+//find <textarea> + simulate "change" event (USE HTML EVENT NAME, NOT REACT!)
+
+wrapped.find('textarea').simulate('change', {
+	//provide fake event object (mock) as 2nd arg, gets merged with event object that passes to other handlers (so passed as arg to handleChange (event.target.value="newcomment", set state etc))
+	target: {value: 'new comment'}
+})
+//force component update
+//assert textarea value changed
 
 })
 
