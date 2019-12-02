4f6ed5162119fb892e04d610af8621a74317a53f
magic8baller <lturis@gmail.com>
2019-12-02 17:37:58 -0500

    setting up comment box component

--- testing/src/components/CommentBox.js
+++ testing/src/components/CommentBox.js
@@ -1,10 +1,36 @@
-import React from 'react'
-const CommentBox = () => {
-	return (
-		<div style={{border: '2px solid blue', margin: '2em'}}>
-			Comment Box
-		</div>
-	)
+import React, {Component} from 'react';
+class CommentBox extends Component {
+
+	state = {comment: ''}
+
+	handleChange = (e) => {
+		this.setState({comment: e.target.value})
+	}
+
+	handleSubmit = e => {
+		e.preventDefault()
+		//call action creator
+		//save comment
+
+	}
+	render () {
+		return (
+			<div style={{
+				border: '2px solid blue',
+				margin: '2em',
+				padding: '1em'
+			}}>
+				<form onSubmit={this.handleSubmit}>
+					<h4>Add A Comment</h4>
+					<textarea onChange={this.handleChange} value={this.state.comment} name="" id="" cols="30" rows="10" />
+					<div>
+						<button type='submit'>Submit</button>
+					</div>
+				</form>
+			</div>
+		);
+	}
+
 }
 
 
--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -5,3 +5,11 @@ import CommentBox from 'components/CommentBox'
 it('shows textarea and button', () => {
 
 })
+
+it('allows user to input text into textarea and submit', () => {
+
+})
+
+it('should clear textarea on input submit', () => {
+	
+})
