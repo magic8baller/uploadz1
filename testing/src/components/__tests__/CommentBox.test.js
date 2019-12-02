f7b52dc73172504581252c46e957450a2a15622f
magic8baller <lturis@gmail.com>
2019-12-02 18:34:28 -0500

    update tests to use describe, DRY

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -14,38 +14,28 @@ afterEach(() => {
 	wrapped.unmount()
 })
 
+// console.log(wrapped.find("textarea").length);
 it('has a textarea and a button', () => {
-	// console.log(wrapped.find("textarea").length);
 	expect(wrapped.find("textarea").length).toEqual(1)
 	expect(wrapped.find("button").length).toEqual(1)
 })
-
-it('allows user to input text into textarea', () => {
-
-	wrapped.find('textarea').simulate('change', {
-
-		target: {value: 'new comment'}
+describe('the textarea', () => {
+	beforeEach(() => {
+		wrapped.find('textarea').simulate('change', {
+			target: {value: 'new comment'}
+		})
+		wrapped.update()
 	})
 
-	wrapped.update()
+	it('allows user to edit text in textarea', () => {
+		expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
 
-	expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
-
-})
-
-it('should clear textarea on form submit', () => {
-//enter textarea
-	wrapped.find('textarea').simulate('change', {
-		target: {value: 'new comment'}
 	})
-	wrapped.update()
-	
-	expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
 
-	wrapped.find('form').simulate('submit')
-	wrapped.update()
+	it('should clear textarea on form submit', () => {
+		wrapped.find('form').simulate('submit')
+		wrapped.update()
+		expect(wrapped.find('textarea').prop('value')).toEqual('')
 
-	expect(wrapped.find('textarea').prop('value')).toEqual('')
-//submit
-//value now is empty string
+	})
 })
