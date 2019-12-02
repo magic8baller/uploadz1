22669a4c1ab1d6a7b6aa31b7430f63a37e2bacc5
magic8baller <lturis@gmail.com>
2019-12-02 18:20:14 -0500

    simulate submit event

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import {mount, unmount} from 'enzyme'
-import CommentBox from 'components/CommentBox'
+import CommentBox from 'components/CommentBox';
+import {mount} from 'enzyme';
+import React from 'react';
 
 
 
 let wrapped;
 
 beforeEach(() => {
-	wrapped = mount(<CommentBox/>)
+	wrapped = mount(<CommentBox />)
 })
 
 afterEach(() => {
@@ -21,20 +21,31 @@ it('has a textarea and a button', () => {
 })
 
 it('allows user to input text into textarea', () => {
-//simulate events:
-//find <textarea> + simulate "change" event (USE HTML EVENT NAME, NOT REACT!)
 
-wrapped.find('textarea').simulate('change', {
-	//provide fake event object (mock) as 2nd arg, gets merged with event object that passes to other handlers (so passed as arg to handleChange (event.target.value="newcomment", set state etc))
-	target: {value: 'new comment'}
-})
-//force component update: but setState = async re-render, but here in test need to do an automatic re-render to check now
-wrapped.update()
-//assert textarea value changed (textarea received the correct value prop!)
-expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
+	wrapped.find('textarea').simulate('change', {
+
+		target: {value: 'new comment'}
+	})
+
+	wrapped.update()
+
+	expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
 
 })
 
 it('should clear textarea on form submit', () => {
+//enter textarea
+	wrapped.find('textarea').simulate('change', {
+		target: {value: 'new comment'}
+	})
+	wrapped.update()
+	
+	expect(wrapped.find('textarea').prop('value')).toEqual('new comment')
 
+	wrapped.find('form').simulate('submit')
+	wrapped.update()
+
+	expect(wrapped.find('textarea').prop('value')).toEqual('')
+//submit
+//value now is empty string
 })
